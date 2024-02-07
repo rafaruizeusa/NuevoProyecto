096bc8a50a5066a57a9344cb4b55b2eb4c41b818
rafaruizeusa <rafa.ruiz@eusa.es>
2024-02-07 19:22:28 +0100

    Mejora de la ventana login realizada

--- MODELO/config.php
+++ MODELO/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', 'localhost');
+define('DB_HOST', '172.45.3.107:9000');
 define('DB_USER', 'user_dwes');
 define('DB_PASSWORD', 'userUSER2');
 define('DB_NAME', 'dwes');
--- VISTA/vistaEditar.php
+++ VISTA/vistaEditar.php
@@ -1,34 +1,10 @@
 <?php
 if ($resultados) {
     ?>
-    <!-- Formulario para editar el producto -->
-    <form action="./CONTROLADOR/controlador.php" method="post">
-        <p>
-            <label for="nombreCorto">Nombre Corto:</label>
-            <input type="text" name="nombreCorto" value="<?php echo htmlspecialchars($nombreCorto) ?>">
-        </p>
 
-        <p>
-            <label for="nombre">Nombre:</label>
-            <input type="text" name="nombre" value="<?php echo htmlspecialchars($resultados['NOMBRE']) ?>">
-        </p>
-
-        <p>
-            <label for="descripcion">Descripción:</label><br>
-            <textarea name="descripcion" rows="4"
-                cols="50"><?php echo htmlspecialchars($resultados['DESCRIPCION']) ?></textarea>
-        </p>
-
-        <p>
-            <label for="pvp">PVP:</label>
-            <input type="text" name="pvp" value="<?php echo $resultados['PVP'] ?>">
-        </p>
-        <p>
-            <!-- Botón para actualizar el producto -->
-            <button id="actualizar" name="nombreEnDB"
-                value="<?php echo htmlspecialchars($nombreCorto) ?>">Actualizar</button>
-        </p>
-    </form>
+    <div>
+        <h1>ESTO ES UN TÍTULO</h1>
+    </div>
 
     <!-- Formulario para cancelar y volver al listado -->
     <form action="./CONTROLADOR/controlador.php">
